85368075790a104fea82568650597f45bf51f729
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-12-05 21:39:51 +0000

    Fix lengthy line

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    """Returns a tuple containing the length of sentence and the first character"""
+    """
+    Returns a tuple containing the length of
+    sentence and the first character
+    """
     return (len(sentence), sentence[0] if len(sentence) else None)
