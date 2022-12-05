f1a0dcc559a631fd0fb24e2c642457ffb90f3edf
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-12-05 21:47:18 +0000

    Another solution task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints a two order list"""
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=(' ' if j != 2 else ''))
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
         print("")
