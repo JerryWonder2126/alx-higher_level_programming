b35747b820f3183f45b4391c0d91e0c5c03e3b8c
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-12-07 22:13:19 +0000

    Back to previous solution

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,43 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    """Convert numerals to integer"""
-    if type(roman_string) == str:
-        roman_ref = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                     'M': 1000}
-        sum = 0
-        flag = 0
-        for i in range(len(roman_string)):
-            if flag:
-                flag = 0
+    """A function that converts a Roman numeral to an integer."""
+
+    if type(roman_string) != str:
+        return 0
+
+    standard = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    extended = {
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900
+    }
+
+    string_len = len(roman_string)
+    end = string_len
+    number = 0
+
+    while end >= 1:
+        if end >= 2:
+            two_character = roman_string[end - 2: end]
+            if two_character in extended:
+                number += extended[two_character]
+                end -= 2
+                print("{} - {}".format(two_character, extended[two_character]))
                 continue
-            for key in roman_ref:
-                if roman_string[i] == key and i != len(roman_string) - 1:
-                    if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
-                        sum += 4
-                        flag = 1
-                    elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
-                        sum += 9
-                        flag = 1
-                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'L':
-                        sum += 40
-                        flag = 1
-                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'C':
-                        sum += 90
-                        flag = 1
-                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'D':
-                        sum += 400
-                        flag = 1
-                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'M':
-                        sum += 900
-                        flag = 1
-                    else:
-                        sum += roman_ref[key]
-                elif roman_string[i] == key:
-                    sum += roman_ref[key]
-        return sum
-    return 0
+        character = roman_string[end - 1]
+        number += standard[character]
+        end -= 1
+
+    return number
