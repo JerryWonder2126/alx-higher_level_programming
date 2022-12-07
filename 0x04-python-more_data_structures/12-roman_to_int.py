01774823998add5f062cfaeb375ef94455244f29
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-12-07 22:12:18 +0000

    Add description

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
 def roman_to_int(roman_string):
+    """Convert numerals to integer"""
     if type(roman_string) == str:
         roman_ref = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                      'M': 1000}
