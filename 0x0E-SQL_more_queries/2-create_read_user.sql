8ca720bc76feb157a5c1809297863812bce41bc0
Jerry Wonder <jerrycul2001@gmail.com>
2023-02-16 23:57:17 +0000

    Do task two

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates user and database
+-- Then grant select privilege to user on database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
