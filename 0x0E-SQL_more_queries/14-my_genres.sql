d537cb71d247207aec81a14685cd701b4c378f2e
Jerry Wonder <jerrycul2001@gmail.com>
2023-02-17 10:05:11 +0000

    Complete remaining compulsory tasks

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT tv_genres.name
+FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_shows.title = 'Dexter' AND tv_show_genres.show_id = tv_shows.id AND tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_genres.name ASC;
