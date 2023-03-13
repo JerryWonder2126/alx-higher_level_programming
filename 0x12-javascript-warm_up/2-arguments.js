6c3da50ef5f580163a5e779e2d73b91c55d93f09
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-13 16:09:59 +0000

    Make code semistandard compliant

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,5 +3,5 @@
 const args = process.argv.slice(2);
 
 if (!args.length) console.log('No argument');
-else if (args.length == 1) console.log('Argument found');
+else if (args.length === 1) console.log('Argument found');
 else console.log('Arguments found');
