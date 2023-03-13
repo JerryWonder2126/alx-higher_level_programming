8cda59d2b809f140762407ef11da5d4584d83635
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-13 16:43:53 +0000

    Complete task 9

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+msg.forEach(value => console.log(value));
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const var1 = parseInt(process.argv[2]);
+if (!var1) console.log('Missing number of occurrences');
+for (let i = 0; i < var1; i++) {
+  console.log('C is fun');
+}
