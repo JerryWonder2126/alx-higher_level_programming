c6da7eefa2017912b0b2ef06b6344e3f87124492
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-13 17:50:36 +0000

    Done with all tasks

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const callMeMoby = (freq, callback) => {
+  while (freq) {
+    callback();
+    freq = freq - 1;
+  }
+}
+
+module.exports = { callMeMoby }
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const addMeMaybe = (num, callback) => callback(num + 1);
+
+module.exports = { addMeMaybe }
