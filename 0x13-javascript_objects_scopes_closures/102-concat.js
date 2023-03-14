58d607b5e96cedd58108ea3ac4e6c3419bc4cdcf
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-14 14:59:53 +0000

    Remove error checks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,13 +3,6 @@
 * Writes the content of two files into an output file
 */
 const fs = require('fs');
-const filePaths = process.argv.slice(2);
-if (!filePaths.length === 3) {
-  const [file1, file2, output] = filePaths;
-  try {
-    const fileContents = [file1, file2].map(value => fs.readFileSync(value, 'utf-8'));
-    fs.writeFileSync(output, fileContents.join(''));
-  } catch (err) {
-    console.log(err.message);
-  }
-}
+const [file1, file2, output] = process.argv.slice(2);
+const fileContents = [file1, file2].map(value => fs.readFileSync(value, 'utf-8'));
+fs.writeFileSync(output, fileContents.join(''));
