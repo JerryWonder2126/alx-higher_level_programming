556eea6837ca51881cac79d59f989f29f750d19e
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-30 19:09:09 +0100

    fix: add fix for task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -ifs -X OPTIONS $1 | grep -i -E 'allow-methods:|allow:' | awk '{ print $2  }'
+curl -is -X OPTIONS $1 | grep -i -E 'allow-methods:|allow:' | awk -F ":" '{ print $2  }' | cut -d ' ' -f 2-
