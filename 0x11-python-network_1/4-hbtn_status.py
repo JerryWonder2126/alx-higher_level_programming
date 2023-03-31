1fc5b19b8835e0e04ef5922bf9986a7e51ada323
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-31 14:39:39 +0100

    chore: complete task 5

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+if __name__ == '__main__':
+    with requests.get('https://alx-intranet.hbtn.io/status') as resp:
+        print('\t- type: {}'.format(type(resp.text)))
+        print('\t- content: {}'.format(resp.text))
